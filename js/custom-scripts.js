3c1abc527fecc3c1fcb25f4e9ba18f5e3208b96b
Omar Afifi <omarafifi137@gmail.com>
2025-09-03 23:36:45 +0300

    Added About Section and edited in css, JS

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,10 +1,33 @@
 const navbar = document.querySelector('.navbar');
-const heroHeight = document.querySelector('.hero').offsetHeight;
+const hero = document.querySelector('.hero');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > heroHeight) {
+function toggleNavbar() {
+    const heroBottom = hero.getBoundingClientRect().bottom;
+
+    if (heroBottom <= 0) {
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
     }
-});
+}
+
+window.addEventListener('scroll', toggleNavbar);
+window.addEventListener('resize', toggleNavbar);
+
+
+(function () {
+    'use strict'
+
+    var forms = document.querySelectorAll('.needs-validation')
+
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
