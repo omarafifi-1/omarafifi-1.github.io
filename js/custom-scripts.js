d5be634e5c5b3319936a1a294a4517bd894ab4c6
Omar Afifi <omarafifi137@gmail.com>
2025-09-05 01:19:16 +0300

    Added Projects Section

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,3 +1,4 @@
+// Auto hide/show navbar on scroll
 const navbar = document.querySelector('.navbar');
 const hero = document.querySelector('.hero');
 
@@ -10,11 +11,10 @@ function toggleNavbar() {
         navbar.classList.remove('scrolled');
     }
 }
-
 window.addEventListener('scroll', toggleNavbar);
 window.addEventListener('resize', toggleNavbar);
 
-
+// Form validation
 (function () {
     'use strict'
 
@@ -31,3 +31,17 @@ window.addEventListener('resize', toggleNavbar);
             }, false)
         })
 })()
+
+// Projects pagination
+const pages = document.querySelectorAll('.project-page');
+const pageLinks = document.querySelectorAll('.page-link');
+  pageLinks.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const pageIndex = e.target.getAttribute('data-page');
+      pages.forEach(p => p.classList.add('d-none'));
+      pages[pageIndex].classList.remove('d-none');
+      document.querySelectorAll('.page-item').forEach(li => li.classList.remove('active'));
+      e.target.parentElement.classList.add('active');
+    });
+  });
